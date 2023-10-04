7ef91ae97c906db7fb0d0138bb8e3c865c0388e4
SatyamKumar <kumarsatyam04.2000@gmail.com>
2023-10-04 08:53:18 +0530

    api chnaged

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import Nav from './components/Nav';
 import CarDisplay from './CarDisplay';
 import data from './cars';
+import carsData from './data'
 
 function App() {
-  const [cars,setCars] = useState(data);
+  const [cars,setCars] = useState(carsData);
   return (
     <>
       <Nav/>
